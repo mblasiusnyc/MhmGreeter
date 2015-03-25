98b4a62ae5828f72ae91baef8ba7e44b9092b8db
Daniel Packard <pack3754@gmail.com>
2015-03-25 08:57:35 -0600

    Add initial structure for greeter application

--- MhmGreeter/Program.cs
+++ MhmGreeter/Program.cs
@@ -6,7 +6,49 @@ namespace MhmGreeter
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			PrintWelcomeScreen ();
+			while (true) {
+				var selection = Console.ReadKey ();
+				if (selection.KeyChar == 'x' || selection.KeyChar == 'X') { 
+					break;
+				}
+
+				Console.WriteLine ();
+				Console.WriteLine ("You selected: " + selection.KeyChar);
+
+				PrintMenuWithPrompt ();
+			}
+
+			Console.WriteLine ("Goodbye!");
+		}
+
+		static void PrintWelcomeScreen ()
+		{
+			Console.WriteLine ("Welcome to MhmGreeter!");
+			PrintMenuWithPrompt ();
+		}
+
+		static void PrintMenu ()
+		{
+			Console.WriteLine ("Please select a greeting:");
+			Console.WriteLine ("  a) English");
+			Console.WriteLine ("  b) Dog");
+			Console.WriteLine ("  c) Cat");
+			Console.WriteLine ("  d) French");
+			Console.WriteLine ("  e) Multilingual");
+			Console.WriteLine ("  f) Random");
+			Console.WriteLine ("  g) Impatient");
+		}
+
+		static void PrintPrompt ()
+		{
+			Console.Write ("> ");
+		}
+
+		static void PrintMenuWithPrompt ()
+		{
+			PrintMenu ();
+			PrintPrompt ();
 		}
 	}
 }
