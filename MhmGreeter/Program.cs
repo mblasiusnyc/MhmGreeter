76211d83dada7ddc24f499129bb183713911b6d8
Daniel Packard <pack3754@gmail.com>
2015-03-26 08:11:22 -0600

    Clean out program.cs -- starting fresh with a pseudocode outline

--- MhmGreeter/Program.cs
+++ MhmGreeter/Program.cs
@@ -6,49 +6,16 @@ namespace MhmGreeter
 	{
 		public static void Main (string[] args)
 		{
-			PrintWelcomeScreen ();
-			while (true) {
-				var selection = Console.ReadKey ();
-				if (selection.KeyChar == 'x' || selection.KeyChar == 'X') { 
-					break;
-				}
+			// PRINT WELCOME SCREEN
 
-				Console.WriteLine ();
-				Console.WriteLine ("You selected: " + selection.KeyChar);
+			// INITIAL PROMPT
+			// LOOP
+			//   READ USER INPUT
+			//   ACT ON INPUT
+			//   PROMPT WITH EXIT
+			// END
 
-				PrintMenuWithPrompt ();
-			}
-
-			Console.WriteLine ("Goodbye!");
-		}
-
-		static void PrintWelcomeScreen ()
-		{
-			Console.WriteLine ("Welcome to MhmGreeter!");
-			PrintMenuWithPrompt ();
-		}
-
-		static void PrintMenu ()
-		{
-			Console.WriteLine ("Please select a greeting:");
-			Console.WriteLine ("  a) English");
-			Console.WriteLine ("  b) Dog");
-			Console.WriteLine ("  c) Cat");
-			Console.WriteLine ("  d) French");
-			Console.WriteLine ("  e) Multilingual");
-			Console.WriteLine ("  f) Random");
-			Console.WriteLine ("  g) Impatient");
-		}
-
-		static void PrintPrompt ()
-		{
-			Console.Write ("> ");
-		}
-
-		static void PrintMenuWithPrompt ()
-		{
-			PrintMenu ();
-			PrintPrompt ();
+			// EXIT
 		}
 	}
 }
