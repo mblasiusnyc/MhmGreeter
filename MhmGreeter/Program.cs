cc28652f049bfe909c256862f7f3bf44f66aaf2c
Daniel Packard <pack3754@gmail.com>
2015-03-26 08:41:58 -0600

    Setup main application loop

--- MhmGreeter/Program.cs
+++ MhmGreeter/Program.cs
@@ -6,16 +6,68 @@ namespace MhmGreeter
 	{
 		public static void Main (string[] args)
 		{
-			// PRINT WELCOME SCREEN
+			PrintWelcomeScreen ();
 
-			// INITIAL PROMPT
-			// LOOP
-			//   READ USER INPUT
-			//   ACT ON INPUT
-			//   PROMPT WITH EXIT
-			// END
+			PrintInitialMenuWithPrompt ();
+			while(true) 
+			{
+				var greetingSelection = ReadGreetingSelection ();
 
-			// EXIT
+				//   ACT ON INPUT
+
+				// quit
+				if (greetingSelection.ToLower () == "x") {
+					break;
+				}
+
+				PrintLoopingMenuWithPrompt();
+			}
+		}
+
+		static void PrintWelcomeScreen ()
+		{
+			Console.WriteLine ("Welcome to MhmGreeter!");
+		}
+
+		static void PrintGreetingMenu ()
+		{
+			// TODO: print all greeting options
+		}
+
+		static void PrintExitOption ()
+		{
+			Console.WriteLine ("x) Exit the application");
+		}
+
+		static void PrintPrompt ()
+		{
+			Console.Write (">> ");
+		}
+
+		static void PrintInitialMenuWithPrompt ()
+		{
+			Console.WriteLine ("Please select one of the following Greetings:");
+			PrintGreetingMenu ();
+			PrintPrompt ();
+		}
+
+		static void PrintLoopingMenuWithPrompt ()
+		{
+			Console.WriteLine ("Please select one of the following Greetings ('X' to exit):");
+			PrintGreetingMenu ();
+			PrintExitOption ();
+			PrintPrompt ();
+		}
+
+		static string ReadGreetingSelection ()
+		{
+			var input = Console.ReadLine();
+			return input;
+		}
+
+		static bool userWantsToExit (string greetingSelection)
+		{
+			return greetingSelection.ToLower () == "x";
 		}
 	}
 }
