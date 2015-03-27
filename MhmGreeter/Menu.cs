5c5cd7f286f8333b197489631ca4c98e44f47a04
Daniel Packard <pack3754@gmail.com>
2015-03-26 20:24:14 -0600

    Factor Menu functionality out into a separate class

---
+++ MhmGreeter/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MhmGreeter
+{
+	class Menu
+	{
+		public Menu()
+		{
+			
+		}
+
+		public void PrintWelcomeScreen ()
+		{
+			Console.WriteLine ("Welcome to MhmGreeter!");
+		}
+
+		public void PrintGreetingMenu ()
+		{
+			// TODO: print all greeting options
+		}
+
+		public void PrintExitOption ()
+		{
+			Console.WriteLine ("x) Exit the application");
+		}
+
+		public void PrintPrompt ()
+		{
+			Console.Write (">> ");
+		}
+
+		public void PrintInitialMenuWithPrompt ()
+		{
+			Console.WriteLine ("Please select one of the following Greetings:");
+			PrintGreetingMenu ();
+			PrintPrompt ();
+		}
+
+		public void PrintLoopingMenuWithPrompt ()
+		{
+			Console.WriteLine ("Would you like to select another greeting? ('X' to exit):");
+			PrintGreetingMenu ();
+			PrintExitOption ();
+			PrintPrompt ();
+		}
+
+		public string ReadGreetingSelection ()
+		{
+			var input = Console.ReadLine ();
+			return input;
+		}
+	}
+}
--- MhmGreeter/Program.cs
+++ MhmGreeter/Program.cs
@@ -6,63 +6,20 @@ namespace MhmGreeter
 	{
 		public static void Main (string[] args)
 		{
-			PrintWelcomeScreen ();
+			Menu menu = new Menu ();
+			menu.PrintWelcomeScreen ();
 
-			PrintInitialMenuWithPrompt ();
-			while(true) 
-			{
-				var greetingSelection = ReadGreetingSelection ();
-
-				//   ACT ON INPUT
+			menu.PrintInitialMenuWithPrompt ();
+			while (true) {
+				var greetingSelection = menu.ReadGreetingSelection ();
 
 				// quit
 				if (greetingSelection.ToLower () == "x") {
 					break;
 				}
 
-				PrintLoopingMenuWithPrompt();
+				menu.PrintLoopingMenuWithPrompt ();
 			}
-		}
-
-		static void PrintWelcomeScreen ()
-		{
-			Console.WriteLine ("Welcome to MhmGreeter!");
-		}
-
-		static void PrintGreetingMenu ()
-		{
-			// TODO: print all greeting options
-		}
-
-		static void PrintExitOption ()
-		{
-			Console.WriteLine ("x) Exit the application");
-		}
-
-		static void PrintPrompt ()
-		{
-			Console.Write (">> ");
-		}
-
-		static void PrintInitialMenuWithPrompt ()
-		{
-			Console.WriteLine ("Please select one of the following Greetings:");
-			PrintGreetingMenu ();
-			PrintPrompt ();
-		}
-
-		static void PrintLoopingMenuWithPrompt ()
-		{
-			Console.WriteLine ("Please select one of the following Greetings ('X' to exit):");
-			PrintGreetingMenu ();
-			PrintExitOption ();
-			PrintPrompt ();
-		}
-
-		static string ReadGreetingSelection ()
-		{
-			var input = Console.ReadLine();
-			return input;
 		}
 
 		static bool userWantsToExit (string greetingSelection)
